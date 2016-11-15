352f54753daecfaca005eac849358b6609b9eaf4
LOGAN NASH <lwnash45@gmail.com>
2016-11-15 15:13:05 -0800

    participation

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,7 +2,7 @@
 
 # Set your directory
 setwd('~/Documents/info-201/m14-shiny/exercise-1/')
-df <- read.csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_us_cities.csv')
+df <- read.csv('https://raw.githubusercontent.com/platasets/master/2014_us_cities.csv')
 
 # Load your buildMap.R Script
 
--- exercise-2/server.R
+++ exercise-2/server.R
@@ -2,7 +2,7 @@
 
 library(shiny)
 
-# We'll look into these more next week: http://shiny.rstudio.com/gallery/widget-gallery.html 
+# We'll look into these more next week: http//shiny.rstudio.com/gallery/widget-gallery.html 
 
 # Create a shiny server that creates a scatterplot. 
 
